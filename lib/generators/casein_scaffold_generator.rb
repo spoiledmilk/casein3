9b86ccbefb76777ac88415048953b7f2f8458fdc
Jamie Appleseed <jamieappleseed@jamie-appleseeds-imac.local>
2010-04-26 17:03:42 +0200

    View file generators.

--- lib/generators/casein_scaffold_generator.rb
+++ lib/generators/casein_scaffold_generator.rb
@@ -1,8 +1,14 @@
 class CaseinScaffoldGenerator < Rails::Generators::NamedBase
+  argument :attributes, :type => :array, :required => true, :desc => "required"
+  
   def self.source_root
     @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
   end
-  def create_controller
+  def generate_files
     template 'controller.rb', "app/controllers/casein/#{plural_name}_controller.rb"
+    template 'views/index.html.erb', "app/views/casein/#{plural_name}/index.html.erb"
+    template 'views/show.html.erb', "app/views/casein/#{plural_name}/show.html.erb"
+    template 'views/new.html.erb', "app/views/casein/#{plural_name}/new.html.erb"
+    template 'views/_fields.html.erb', "app/views/casein/#{plural_name}/_fields.html.erb"
   end
 end
