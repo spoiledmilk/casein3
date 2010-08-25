461575b7d86a1bbd1faf9c6b0297d2d391d34c07
Russell Quinn <russell@mcsweeneys.net>
2010-08-25 14:42:47 +0200

    Another attempt to fix include problem

--- app/mailers/casein/casein_notification.rb
+++ app/mailers/casein/casein_notification.rb
@@ -2,33 +2,29 @@ module Casein
   
   class CaseinNotification < ActionMailer::Base
 	
-		helper Casein::ConfigHelper
-	
-  	default :from => Casein::ConfigHelper.casein_config_email_from_address
-
   	self.prepend_view_path File.join(File.dirname(__FILE__), '..', 'views', 'casein')
 	
-  	def generate_new_password casein_user, host, pass
+  	def generate_new_password from, casein_user, host, pass
   		@name = casein_user.name
   		@host = host
   		@login = casein_user.login
   		@pass = pass
   		@from_text = casein_config_website_name
   		
-  		mail(:to => casein_user.email, :subject => "[#{casein_config_website_name}] New password")
+  		mail(:to => casein_user.email, :from => from, :subject => "[#{casein_config_website_name}] New password")
   	end
   
-  	def new_user_information casein_user, host, pass
+  	def new_user_information from, casein_user, host, pass
       @name = casein_user.name
   		@host = host
   		@login = casein_user.login
   		@pass = pass
   		@from_text = casein_config_website_name
   		
-  		mail(:to => casein_user.email, :subject => "[#{casein_config_website_name}] New user account")
+  		mail(:to => casein_user.email, :from => from, :subject => "[#{casein_config_website_name}] New user account")
   	end
   	
-  	def password_reset_instructions casein_user, host
+  	def password_reset_instructions from, casein_user, host
   	  ActionMailer::Base.default_url_options[:host] = host.gsub("http://", "")
       @name = casein_user.name
       @host = host
@@ -36,7 +32,7 @@ module Casein
       @reset_password_url = edit_casein_password_reset_url + "/?token=#{casein_user.perishable_token}"
       @from_text = casein_config_website_name
 
-      mail(:to => casein_user.email, :subject => "[#{casein_config_website_name}] Password reset instructions")
+      mail(:to => casein_user.email, :from => from, :subject => "[#{casein_config_website_name}] Password reset instructions")
     end
 
   end
--- app/models/casein/user.rb
+++ app/models/casein/user.rb
@@ -28,19 +28,19 @@ module Casein
     end
 	
   	def send_create_notification
-      Casein::CaseinNotification.new_user_information(self, casein_config_hostname, @password).deliver
+      Casein::CaseinNotification.new_user_information(casein_config_email_from_address, self, casein_config_hostname, @password).deliver
     end
   
     def send_update_notification
       if notify_of_new_password
         notify_of_new_password = false
-        Casein::CaseinNotification.generate_new_password(self, casein_config_hostname, @password).deliver
+        Casein::CaseinNotification.generate_new_password(casein_config_email_from_address, self, casein_config_hostname, @password).deliver
       end
     end
     
     def send_password_reset_instructions
       reset_perishable_token!
-      Casein::CaseinNotification.password_reset_instructions(self, casein_config_hostname).deliver
+      Casein::CaseinNotification.password_reset_instructions(casein_config_email_from_address, self, casein_config_hostname).deliver
     end
     
     def check_time_zone
